fb4f8e0624803b1b23521c2327250f3caddfb4a0
Ahmed Hossam <ahmedhossam9226@gmail.com>
2024-02-12 19:46:48 +0200

    hanlde JWT errors

--- middlewares/isLogin.js
+++ middlewares/isLogin.js
@@ -3,21 +3,26 @@ const createError = require('http-errors');
 const User = require('../models/User');
 const getTokenFromHeader = require('../utils/getTokenFromHeader');
 
-const isLogin = async (req, res, next) => { 
+const isLogin = async (req, res, next) => {
   const token = getTokenFromHeader(req);
   if (!token) {
     return next(createError(401, 'Unauthorized'));
   }
   const decoded = verifyToken(token);
+
+  if (decoded === 'TokenExpiredError') {
+    return next(createError(401, 'Token expired'));
+  }
   if (!decoded) {
     return next(createError(401, 'Unauthorized'));
   }
+
   const user = await User.findById(decoded.userId);
   if (!user) {
     return next(createError(401, 'Unauthorized'));
   }
   req.user = user;
   next();
-}
+};
 
 module.exports = isLogin;
--- utils/verifyToken.js
+++ utils/verifyToken.js
@@ -1,12 +1,21 @@
 const jwt = require('jsonwebtoken');
 
 const verifyToken = (token) => {
-  try {
-    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-    return decoded;
-  } catch (error) {
-    return false;
-  }
+  const decoded = jwt.verify(
+    token,
+    process.env.ACCESS_TOKEN_SECRET,
+    (err, decoded) => {
+      if (err) {
+        if (err.name === 'TokenExpiredError') {
+          return 'TokenExpiredError';
+        }
+
+        return false;
+      }
+      return decoded;
+    }
+  );
+  return decoded;
 };
 
 module.exports = verifyToken;
